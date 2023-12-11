c13978bd5929bbda5f642a9c3d6838779122e7bc
KonkenBonken <konrad05pettersson@gmail.com>
2023-12-11 18:36:58 +0100

    add score

--- src/Main.tsx
+++ src/Main.tsx
@@ -6,6 +6,7 @@ import Block from './components/Block';
 
 export default function Main() {
   const [grid, setGrid] = useState<(boolean | 'destroyed')[][]>(Array(10).fill(0).map(() => Array(10).fill(false)));
+  const [score, setScore] = useState(0);
 
   function placeBlock(shape: boolean[][], x: number, y: number) {
     for (let row = 0; row < shape.length; row++)
@@ -16,6 +17,8 @@ export default function Main() {
     for (let row = 0; row < shape.length; row++)
       for (let cell = 0; cell < shape[row].length; cell++)
         grid[y + row][x + cell] ||= shape[row][cell];
+
+    setScore(prev => prev + shape.flat().filter(Boolean).length ** 2);
 
     for (let row = 0; row < 10; row++)
       if (grid[row].every(Boolean)) {
@@ -44,6 +47,7 @@ export default function Main() {
     <Board grid={grid} />
     <div className={scss.panel}>
       <h1>Blockz</h1>
+      <h6>{score}</h6>
       <Block placeBlock={placeBlock} n={0} />
       <Block placeBlock={placeBlock} n={1} />
       <Block placeBlock={placeBlock} n={2} />
